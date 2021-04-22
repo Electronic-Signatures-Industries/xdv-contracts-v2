786d70203497b150cbd3ca38d19654e276f4d6f6
Roberto E. Zubieta <roberto.zubieta@admios-sa.com>
2021-04-22 15:27:00 -0500

    Removed Unused Import

--- contracts/XDVToken.sol
+++ contracts/XDVToken.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract XDVToken is
